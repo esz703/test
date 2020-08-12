f3cc845edc9881fe8d5497d8ef56f078c5ad55ac
esz703 <jaho1902@gmail.com>
2020-08-12 22:00:34 +0900

    두번째 디벨롭 삭제 세번째 디벨럽 추가

--- 0812/src/Main.java
+++ 0812/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("재호 멍충이");
 		System.out.println("이건 디벨롭");
-		System.out.println("두번째 디벨롭");
+		System.out.println("세번째 디벨롭");
 	}
 }
