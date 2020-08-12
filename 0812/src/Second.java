4af1bd83976bd7cfe95be8931b1729596d9c9224
esz703 <jaho1902@gmail.com>
2020-08-12 22:08:05 +0900

    러알넝라ㅣ어라ㅣ너ㅣ

--- 0812/src/Second.java
+++ 0812/src/Second.java
@@ -4,5 +4,11 @@ public class Second {
 		System.out.println("재호 멍충이");
 		System.out.println("이건 디벨롭");
 		System.out.println("세번째 디벨롭");
+		System.out.println("재호 멍충이");
+		System.out.println("이건 디벨롭");
+		System.out.println("세번째 디벨롭");
+		System.out.println("재호 멍충이");
+		System.out.println("이건 디벨롭");
+		System.out.println("세번째 디벨롭");
 	}
 }
